cba87bbc6c1e9f5f58c855b7b2a130f55f2b3b8b
H-A-L-F <arya.jayadi2013@gmail.com>
2024-08-30 13:31:49 +0700

    insertCompanyForm

--- src/presentation/company/InsertCompanyPage.tsx
+++ src/presentation/company/InsertCompanyPage.tsx
@@ -15,35 +15,37 @@ export const InsertCompanyPage = () => {
     } = useViewModel()
 
     return (
-        <Card className="w-[350px]">
-            <CardHeader>
-                <CardTitle>Insert Company</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <form>
-                    <div className="grid w-full items-center gap-4">
-                        <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="name">Name</Label>
-                            <Input id="name" placeholder="Name" ref={nameRef} type="text"/>
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>Insert Company</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="grid w-full items-center gap-4">
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="name">Name</Label>
+                                <Input id="name" placeholder="Name" ref={nameRef} type="text"/>
+                            </div>
+                            <div className="grid w-full max-w-sm items-center gap-1.5">
+                                <Label htmlFor="picture">Picture</Label>
+                                <Input id="picture" type="file" className="cursor-pointer" ref={picRef}/>
+                            </div>
+                            <div className="grid w-full gap-1.5">
+                                <Label htmlFor="description">Description</Label>
+                                <Textarea placeholder="Type your description here." ref={descRef} id="message"/>
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="password">Location</Label>
+                                <Input id="password" placeholder="Password" ref={locRef} type="text"/>
+                            </div>
                         </div>
-                        <div className="grid w-full max-w-sm items-center gap-1.5">
-                            <Label htmlFor="picture">Picture</Label>
-                            <Input id="picture" type="file" className="cursor-pointer" ref={picRef}/>
-                        </div>
-                        <div className="grid w-full gap-1.5">
-                            <Label htmlFor="description">Description</Label>
-                            <Textarea placeholder="Type your description here." ref={descRef} id="message"/>
-                        </div>
-                        <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="password">Location</Label>
-                            <Input id="password" placeholder="Password" ref={locRef} type="text"/>
-                        </div>
-                    </div>
-                </form>
-            </CardContent>
-            <CardFooter className="flex flex-row-reverse">
-                <Button onClick={handleSubmit}>Insert</Button>
-            </CardFooter>
-        </Card>
+                    </form>
+                </CardContent>
+                <CardFooter className="flex flex-row-reverse">
+                    <Button onClick={handleSubmit}>Insert</Button>
+                </CardFooter>
+            </Card>
+        </div>
     )
 }
