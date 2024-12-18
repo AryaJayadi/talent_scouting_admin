62080f01d29e3d2f498d28aa39731278cba4a62e
Arya <64455337+AryaJayadi@users.noreply.github.com>
2024-12-18 14:47:45 +0700

    Create Student.ts

---
+++ src/domain/model/Student.ts
@@ -0,0 +1,17 @@
+export interface Student {
+    id: string;
+    nim: string;
+    name: string;
+    phone: string;
+    gpa: number;
+    major: string;
+    address: string;
+    city: string;
+    state: string;
+    pictureUrl: string;
+    description: string;
+    personalUrl: string;
+
+    userId: string;
+    email: string;
+}
