257c7f7af2929a4e9088478aea880e1b4b1008ff
Arya <64455337+AryaJayadi@users.noreply.github.com>
2024-12-19 09:27:26 +0700

    implemented ProtectedRoute

--- src/core/ApplicationRouter.tsx
+++ src/core/ApplicationRouter.tsx
@@ -1,17 +1,32 @@
 import {
-    createBrowserRouter,
+    createBrowserRouter, Navigate,
     RouterProvider,
 } from "react-router-dom";
 import {Root} from "@/presentation/template/Root.tsx";
 import {LoginPage} from "@/presentation/auth/LoginPage.tsx";
 import {AuthLayout} from "@/presentation/template/AuthLayout.tsx";
-import {HomePage} from "@/presentation/home/HomePage.tsx";
-import {InsertCompanyPage} from "@/presentation/company/InsertCompanyPage.tsx";
-import {BaseLayout} from "@/presentation/template/BaseLayout.tsx";
-import {InsertCompanyBulkPage} from "@/presentation/company/InsertCompanyBulkPage.tsx";
 import StudentPage from "@/app/dashboard/student/page.tsx";
 import {DashboardLayout} from "@/app/dashboard/layout.tsx";
 import {CompanyPage} from "@/presentation/company/CompanyPage.tsx";
+import {Outlet} from "react-router";
+import {FC} from "react";
+
+function isAuthencticated() : boolean {
+    return true;
+}
+
+interface ProtectedRouteProps {
+    isAuthenticated: () => boolean;
+    redirectPath: string;
+}
+
+const ProtectedRoute: FC<ProtectedRouteProps> = (p) => {
+    if(!p.isAuthenticated()) {
+        return <Navigate to={p.redirectPath} replace/>
+    }
+
+    return <Outlet />
+}
 
 const router = createBrowserRouter([
     {
@@ -20,41 +35,24 @@ const router = createBrowserRouter([
         children: [
             {
                 path: "",
-                element: <DashboardLayout />,
-                children:[
+                element: <ProtectedRoute isAuthenticated={isAuthencticated} redirectPath={"/auth/login"} />,
+                children: [
                     {
-                        path: "company",
-                        element: <CompanyPage />
-                    },
-                    {
-                        path: "student",
-                        element: <StudentPage />
+                        path: "",
+                        element: <DashboardLayout />,
+                        children:[
+                            {
+                                path: "company",
+                                element: <CompanyPage />
+                            },
+                            {
+                                path: "student",
+                                element: <StudentPage />
+                            },
+                        ]
                     },
                 ]
             },
-            // {
-            //     path: "",
-            //     element: <BaseLayout />,
-            //     children:[
-            //         {
-            //             path: "",
-            //             element: <HomePage />
-            //         },
-            //         {
-            //             path: "company",
-            //             children: [
-            //                 {
-            //                     path: "insert",
-            //                     element: <InsertCompanyPage />,
-            //                 },
-            //                 {
-            //                     path: "insert/bulk",
-            //                     element: <InsertCompanyBulkPage />,
-            //                 }
-            //             ]
-            //         },
-            //     ]
-            // },
             {
                 path: "auth",
                 element: <AuthLayout />,
