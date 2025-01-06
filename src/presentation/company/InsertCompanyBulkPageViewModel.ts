dcbeab9c1f2949ea34747762d9bc9bd3205b7c00
Arya <64455337+AryaJayadi@users.noreply.github.com>
2025-01-06 14:13:44 +0700

    remove unused pages

--- src/presentation/company/InsertCompanyBulkPageViewModel.ts
+++
@@ -1,9 +0,0 @@
-import {useRef} from "react";
-
-export default function InsertCompanyBulkPageViewModel() {
-    const fileRef = useRef<HTMLInputElement | null>(null);
-
-    return {
-        fileRef,
-    }
-}
